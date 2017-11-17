24bebfb1335753d43ab86dc51e067cab1c41c834
Nikhil <nikskashyap1206@gmail.com>
2017-11-17 13:55:35 +0100

    2.1

---
+++ JavaScriptInleiding/2.1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn subkop";
+document.getElementById("aap1").src = "silverback.jpg";
+document.getElementById("aap2").src = "giantape.jpg";
